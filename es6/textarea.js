c751ec95a551aa022a130b4442448ec134437e83
Admin <Admin@DESKTOP-K15D8KB>
2018-11-12 15:58:52 +0200

    First commit

---
+++ es6/textarea.js
@@ -0,0 +1,18 @@
+// let autoExpand = function (field) {
+//   field.style.height = 'inherit'
+
+//   let computed = window.getComputedStyle(field)
+
+//   let height = parseInt(computed.getPropertyValue('border-top-width'), 10) +
+//             parseInt(computed.getPropertyValue('padding-top'), 10) +
+//             field.scrollHeight +
+//             parseInt(computed.getPropertyValue('padding-bottom'), 10) +
+//             parseInt(computed.getPropertyValue('border-bottom-width'), 10)
+
+//   field.style.height = height + 'px'
+// }
+
+// document.addEventListener('input', function (event) {
+//   if (event.target.tagName.toLowerCase() !== 'textarea') return
+//   autoExpand(event.target)
+// }, false)
